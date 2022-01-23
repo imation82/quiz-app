397bbcd55bd78bc3913dc685142986954c61cbb9
imation82 <odesk.rahman@gmail.com>
2022-01-23 20:31:33 +0600

    Quiz app

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Quiz />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Quiz from './components/Quiz.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Quiz
+  }
+}
+</script>
